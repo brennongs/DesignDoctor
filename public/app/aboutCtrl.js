7f83b85fc558b7d76392d3b5aee82f14b3598b0d
brennongs <brennonschow@gmail.com>
2017-06-04 12:24:45 -0600

    added and styled about pages

---
+++ public/app/aboutCtrl.js
@@ -0,0 +1,23 @@
+angular.module('DesignDoc').controller('aboutCtrl',function($scope, $state, $sce, aboutService){
+
+  $scope.getText=(name, cb)=>{
+    aboutService.getText(name).then((response)=>{
+      cb(response.data)
+    })
+  }
+  $scope.getText('about-me')
+
+
+  $scope.setPage=(url)=>{
+    switch (url) {
+      case '/about-this':
+        $scope.getText('about-quote',(x)=>{$scope.quote=$sce.trustAsHtml(x)})
+        $scope.getText('quote-author',(x)=>{$scope.author=$sce.trustAsHtml(x)})
+        $scope.getText('about-this',(x)=>{$scope.about=$sce.trustAsHtml(x)})
+        break
+      case '/about-me':
+        $scope.getText('about-me',(x)=>{$scope.about=$sce.trustAsHtml(x)})
+    }
+  }
+  $scope.setPage($state.current.url)
+})
